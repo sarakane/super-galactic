82714ca3a3e658938f582c1a1d1d506824c4f74b
Sara Kane <kane.sarak@gmail.com>
2020-06-26 15:15:25 -0700

    Add failing test for 'If the user is older than life expectancy, the program should return the users age and years past expectancy for all planets as a string'

--- __tests__/superGalactic.test.js
+++ __tests__/superGalactic.test.js
@@ -3,10 +3,12 @@ import { SuperGalactic } from './../src/superGalactic.js';
 describe('SuperGalactic', () => {
   let superGalactic;
   let sGAge;
+  let surpassedAverage;
 
   beforeEach(() => {
     superGalactic = new SuperGalactic(27, 81);
     sGAge = superGalactic.age;
+    surpassedAverage = new SuperGalactic(82, 81);
   });
 
   test('Take a persons age and average life expectancy and create a class ', () => {
@@ -35,7 +37,6 @@ describe('SuperGalactic', () => {
   });
 
   test('If user has already surpassed the average life expectancy, return number of years they have live past the life expectancy', () => {
-    let surpassedAverage = new SuperGalactic(82, 81);
     expect(surpassedAverage.yearsPast()).toEqual({mercury: "4.2", venus: "1.7", mars: "0.5", jupiter: "0.1"});
   });
 
@@ -44,7 +45,10 @@ describe('SuperGalactic', () => {
   });
 
   test('The program should return the users age and years left to live for all planets as a string', () => {
-    console.log(superGalactic.runSuperGalactic());  
     expect(superGalactic.runSuperGalactic()).toEqual('Your age on Mercury would be 112.5 and you would have 225.0 years left to live. Your age on Venus would be 43.5 and you would have 87.1 years left to live. Your age on Mars would be 14.4 and you would have 28.7 years left to live. Your age on Jupiter would be 2.3 and you would have 4.5 years left to live.');
+  });
+
+  test('If the user is older than life expectancy, the program should return the users age and years past expectancy for all planets as a string', () => {
+    expect(surpassedAverage.runSuperGalactic()).toEqual("Congratulations! You've surpassed your life expectancy. Your age on Mercury would be 341.7 and you have lived 4.2 years past your life expectancy. Your age on Venus would be 132.3 and you have lived 1.7 years past your life expectancy. Your age on Mars would be 43.6 and you have lived 0.5 years past your life expectancy. Your age on Jupiter would be 6.9 and you have lived 0.1 years past your life expectancy.");
   });
 });
--- src/superGalactic.js
+++ src/superGalactic.js
@@ -60,6 +60,8 @@ export class SuperGalactic{
     if(this.age < this.lifeExpectancy) {
       yearDiff = this.yearsLeft();
       outputString = `Your age on Mercury would be ${planetAges.mercury} and you would have ${yearDiff.mercury} years left to live. Your age on Venus would be ${planetAges.venus} and you would have ${yearDiff.venus} years left to live. Your age on Mars would be ${planetAges.mars} and you would have ${yearDiff.mars} years left to live. Your age on Jupiter would be ${planetAges.jupiter} and you would have ${yearDiff.jupiter} years left to live.`;
+    } else if (this.age < this.lifeExpectancy){
+      
     }
     return outputString;
   }
