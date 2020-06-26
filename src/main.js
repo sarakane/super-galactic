400b2ac04558fd1ed8034d816e109a8dec79a699
Sara Kane <kane.sarak@gmail.com>
2020-06-26 16:19:10 -0700

    Add output area to show returned string

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { SuperGalactic } from './../src/superGalactic.js';
 
 $(document).ready(function() {
   $("form#ageForm").submit(function(event) {
     event.preventDefault();
+    const ageInput = $("#age").val();
+    const lifeExpectancyInput = $("#lifeExpectancy").val();
+    let sG = new SuperGalactic(ageInput, lifeExpectancyInput);
+    $("#output").text(sG.runSuperGalactic());
   });
 });
